4ad7e462a342e09f82c2473716b08fbf3e6702b4
David Igbayilola <Kingigbayilola@gmail.com>
2026-01-21 11:41:39 +0100

    refactor: Clean up code formatting and remove unused styles in main.css

--- membership-allocation-client/electron.vite.config.ts
+++ membership-allocation-client/electron.vite.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react(),  tailwindcss()]
+    plugins: [react(), tailwindcss()]
   }
 })
