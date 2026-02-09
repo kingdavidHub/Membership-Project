3d52ae02b394bdbd04ab4b9a2f798d8609f3437a
Idunnuoluwa Abimbola <thecoder.co@gmail.com>
2026-02-09 15:23:19 +0100

    express

---
+++ membership-backend/routes/authRoutes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
+
+const router = express.Router();
+
+router.post('/login', authController.login);
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+// // Protect all routes after this middleware
+
+// router.use(authController.protect);
+
+// router.patch('/updateMyPassword', authController.updatePassword);
+
+// router.get('/me', userController.getMe, userController.getUser);
+
+// // router.patch(
+// //   '/updateMe',
+// //   userController.uploadUserPhoto,
+// //   userController.resizeUserPhoto,
+// //   userController.updateMe
+// // );
+
+router.use(authController.restrictTo('admin'));
+router.post('/admin/create-user', authController.createUser);
+
+// router
+//   .route('/')
+//   .get(userController.getAllUsers)
+//   .post(userController.createUser);
+
+// router
+//   .route('/:id')
+//   .get(userController.getUser)
+//   .patch(userController.updateUser)
+// .delete(userController.deleteUser);
+
+module.exports = router;
